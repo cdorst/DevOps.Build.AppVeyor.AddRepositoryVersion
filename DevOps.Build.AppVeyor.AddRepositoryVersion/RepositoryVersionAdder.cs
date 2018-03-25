18b4499ce2c83aab78166a3e5855cc11f7e41a04
Christopher Dorst <chris@dorst.email>
2018-03-25 16:51:13 -0500

    Update RepositoryVersionAdder.cs

--- DevOps.Build.AppVeyor.AddRepositoryVersion/RepositoryVersionAdder.cs
+++ DevOps.Build.AppVeyor.AddRepositoryVersion/RepositoryVersionAdder.cs
@@ -1,3 +1,6 @@
+// Copyright © Christopher Dorst. All rights reserved.
+// Licensed under the GNU General Public License, Version 3.0. See the LICENSE document in the repository root for license information.
+
 using Microsoft.WindowsAzure.Storage.Table;
 using System.Threading.Tasks;
 using static DevOps.Build.AppVeyor.AzureStorageTableVersionLedger.Builder.RepositoryVersionTableHelper;
@@ -11,10 +14,10 @@ namespace DevOps.Build.AppVeyor.AddRepositoryVersion
         /// <summary>Adds a table entry to an Azure Table named "appveyor"</summary>
         public static async Task AddRepositoryVersionAsync(string accountName, string repositoryName, string version)
         {
-var entry = RepositoryVersionTableEntry(accountName, repositoryName, version);
-var operation = TableOperation.InsertOrReplace(entry);
-var table = await GetTable();
-await table.ExecuteAsync(operation);;
+            var entry = RepositoryVersionTableEntry(accountName, repositoryName, version);
+            var operation = TableOperation.InsertOrReplace(entry);
+            var table = await GetTable();
+            await table.ExecuteAsync(operation);;
         }
     }
 }
